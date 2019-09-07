822a16e2fd0353c6689cb07dd1f49a5bd3c415a9
Icedgarr <garriga77@gmail.com>
2019-09-07 14:55:12 +0200

    :sparkles: Create space of random variables, parameters and duration generators.

---
+++ adaptation_to_regime_changes/definitions.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# parameters
+maximum_duration = 100
+theta_good = 0.75
+
+# Space of the random variables
+a_t = {0: 'left', 1: 'right'}
+s_t = {0: 'left good', 1: 'right_good'}
+o_t = {0: 'good choice', 1: 'bad choice'}
+d_t = list(range(maximum_duration))
+
+
+# Duration generators
+def generate_regular_d():
+    duration = maximum_duration + 1
+    while duration > maximum_duration:
+        duration = np.random.binomial(40, 0.5, 1)
+    return duration
+
+
+def generate_irregular_d():
+    duration = maximum_duration + 1
+    while duration > maximum_duration:
+        duration = np.random.lognormal(2, 1, 1)
+    return duration
